75b7efaa00764d75ecd1b72e2dc746def9e66b95
David Robertsson <david.robertsson@gmail.com>
2024-04-05 14:40:34 +0200

    Adaption 2016 and added labels.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Author: David Robertsson
+# Date: 2024-04-05
+# Handles input for forms of version "2016"
+
 from glob import glob
 import datetime
 from openpyxl import Workbook, load_workbook
@@ -142,14 +146,83 @@ def append_workbook(wb, file_name, dogs):
 
 
 #    return wb
-
+labels = [
+    "Source.Name",
+    "MV-Beskrivare",
+    "Ras Namn",
+    "Raskod",
+    "RegNr",
+    "Provdatum",
+    "1a Nyfikenhet, Utrymme",
+    "1ab Trygghet Utrymme",
+    "1b Nyfikenhet VB",
+    "1c Hälsning/Kontakt, VB står stilla",
+    "2a Följa efter, VB går omkring, Tyst",
+    "2b Kontakttagande, VB går omkring, Tyst",
+    # "2c Följa efter, VB går omkring o lockar", not in version 2016
+    # "2d Kontakttagande, VB går omkring o lockar", not in version 2016
+    "3a Nyfikenhet, VB sitter på golvet 30sek",
+    "3b Hälsning, VB sitter på golvet",
+    "3c Trygghet, VB sitter på golvet",
+    "4a Hälsning, VB lyfter valpen 10sek",
+    "4b Trygghet, VB lyfter valpen 10sek",
+    "4c Aggression, VB lyfter valpen 10sek",
+    "5a Springa efter, liten boll 1:a",
+    "5a                                2:a",
+    "5a                                3:dje",
+    "5b Gripande/intensitet, liten boll 1:a",
+    "5b                                2:a",
+    "5b                                3:dje",
+    "5c  lek/samarbete, liten boll 1:a",
+    "5c                                2:a",
+    "5c                                3:dje",
+    "6a Nyfikenhet, stor boll 30sek",
+    "6b Lek/Intensitet stor boll",
+    # "6bb Lek/Tid, stor boll", not in version 2016
+    "6c Trygghet, stor boll",
+    "7a Lek/Dragkamp med trasa 30sek",
+    "7b Samarbete trasa",
+    "8a Nyfikenhet Ljud 30sek",
+    "8ab Lek/Intensitet Ljud",
+    "8b Lek/Tid Ljud",
+    "8c Lek Trygghet Ljud",
+    "9a Nyfikenhet 6 föremål 60sek",
+    "9b Föremålslek 6 föremål 60sek",
+    "9c Gripande/form, Repfläta",
+    "10a  Handlingsförmåga, Hinder/Galler 30sek",
+    "10b Trygghet, Hinder/Galler",
+    "10c Strategier, Hinder/Galler",
+    "11a Handlingsförmåga, Underlag 30sek",
+    "11b Trygghet, Underlag",
+    "12a Aktiv/energisk, under mom. skattning av VB",
+    "12ab Koppla av, mellan mom. skattning av VB",
+    "12b Trygg, under test skattning av VB",
+    "13b Akt.nivå i kullen 1-5, Skattning av Uppfödaren",
+    "Visar resursförsvar Ja/Nej",
+    "Gör annat under test 1.2.3.4",
+    "Ljud/Oro, Gnäller/Gläfser/Skäller 1.2.3.4",
+    # "Ljud/frustration,  1.2.3.4", not in version 2016
+    "Avbruten",
+    "Filmat",
+    "Kön",
+    "Född",
+    "Far Namn",
+    "Far Reg.Nr",
+    "Mor Namn",
+    "Mor Reg.Nr",
+    "Kennel",
+    "KullID",
+    "ÅR",
+]
 
 files = list_input()
 wb = Workbook()
+sheet = wb.active
+sheet.append(labels)
 
 for file in files:
     print(".")
     dogs = read_dogs(file)
     append_workbook(wb, file, dogs)
 
-wb.save(f"./output/sammanställning{datetime.datetime.now()}.xlsx")
+wb.save(f"./output/sammanställning_{datetime.datetime.now()}_version2016.xlsx")
